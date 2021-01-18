7519e86a3ea41940e7aac12d4027a80be6f48ed1
kurita-t <mikanesan420@gmail.com>
2021-01-19 00:55:00 +0900

    Active Storageの導入

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,4 +8,8 @@ class ProductsController < ApplicationController
   def move_to_index
     redirect_to action: :index unless user_signed_in?
   end
+
+  def product_params
+    params.require(:product).permit(:image).merge(user_id: current_user.id)
+  end
 end
