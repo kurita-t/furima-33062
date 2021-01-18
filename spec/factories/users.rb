2870bfe6971a8ab29598ad3bb6260fba6f0c11c4
kurita-t <mikanesan420@gmail.com>
2021-01-18 19:40:37 +0900

    Userモデル単体テストコード

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,9 +5,9 @@ FactoryBot.define do
     password = Faker::Internet.password(min_length: 6)
     password {password}
     password_confirmation {password}
-    family_name  {"山田"}
+    family_name {"山田"}
     first_name {"太郎"}
-    family_name_kana  {"ヤマダ"}
+    family_name_kana {"ヤマダ"}
     first_name_kana {"タロウ"}
     birth_day {"2000-01-01"}
   end
