722e4d11575a11604f9dee2575f99b0c3cf4c1f4
kurita-t <mikanesan420@gmail.com>
2021-01-21 23:38:31 +0900

    商品出品機能実装

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,22 @@
 class ProductsController < ApplicationController
-  before_action :move_to_index, except: [:index, :show]
+  #before_action :authenticate_user!, only: [:destroy, :new, :edit]
+  before_action :move_to_index, only: [:edit, :update, :destroy]#except: [:index, :show]
   def index
+    @products = Product.order("created_at DESC")
+  end
+
+  def new
+    @product = Product.new
+
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
@@ -10,6 +26,6 @@ class ProductsController < ApplicationController
   end
 
   def product_params
-    params.require(:product).permit(:image).merge(user_id: current_user.id)
+    params.require(:product).permit(:image, :name, :description, :category_id, :condition_id, :shipping_cost_id, :prefecture_id, :shipping_day_id, :price).merge(user_id: current_user.id)
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -8,7 +8,8 @@ class Product < ApplicationRecord
   belongs_to :prefecture
   belongs_to :shipping_day
 
-  validates :name, :description, :price presence: true
+  validates :name, :description, :price, presence: true
+  validates :price, numericality: { only_integer: true,  greater_than: 300, less_than: 9999999}
 
   #ジャンルの選択が「--」の時は保存できないようにする
   validates :category_id, :condition_id, :shipping_cost_id, :prefecture_id, :shipping_day_id, numericality: { other_than: 1 }
