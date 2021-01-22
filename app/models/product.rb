62956c32a42f2615665ee07ed0e1f4c0a1641325
kurita-t <mikanesan420@gmail.com>
2021-01-22 18:34:39 +0900

    Rubocop実行(商品表示)

--- app/models/product.rb
+++ app/models/product.rb
@@ -9,14 +9,13 @@ class Product < ApplicationRecord
   belongs_to :shipping_day
 
   with_options presence: true do
-    validates :name, length: {minimum: 1, maximum: 40 }
-    validates :description, length: {minimum: 1, maximum: 1000 }
+    validates :name, length: { minimum: 1, maximum: 40 }
+    validates :description, length: { minimum: 1, maximum: 1000 }
     validates :price
     validates :image
   end
-  validates :price, numericality: { only_integer: true,  greater_than: 299, less_than: 9999999}
+  validates :price, numericality: { only_integer: true, greater_than: 299, less_than: 9_999_999 }
 
-  #ジャンルの選択が「--」の時は保存できないようにする
+  # ジャンルの選択が「--」の時は保存できないようにする
   validates :category_id, :condition_id, :shipping_cost_id, :prefecture_id, :shipping_day_id, numericality: { other_than: 1 }
-
 end
