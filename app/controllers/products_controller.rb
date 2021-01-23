23a088fc5ef6d987bce621ac3e72994548119977
kurita-t <mikanesan420@gmail.com>
2021-01-23 21:09:48 +0900

    商品情報編集機能実装

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,6 @@
 class ProductsController < ApplicationController
   before_action :move_to_index, only: [:new, :create, :edit, :update, :destroy]
+  before_action :ensure_current_user, only: [:edit, :update]
   before_action :set_product, only: [:edit, :show]
   def index
     @products = Product.all.order('created_at DESC')
@@ -24,6 +25,15 @@ class ProductsController < ApplicationController
   def show
   end
 
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to product_path
+    else
+      render :edit
+    end
+  end
+
   private
 
   def move_to_index
@@ -33,6 +43,13 @@ class ProductsController < ApplicationController
   def product_params
     params.require(:product).permit(:image, :name, :description, :category_id, :condition_id, :shipping_cost_id, :prefecture_id,
                                     :shipping_day_id, :price).merge(user_id: current_user.id)
+  end
+
+  def ensure_current_user
+    product = Product.find(params[:id])
+    if product.user_id != current_user.id
+      redirect_to action: :index
+    end
   end
 
   def set_product
