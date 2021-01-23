7fdf0fdfa2be2c9ca54d91e9f4188a2ae77c250c
kurita-t <mikanesan420@gmail.com>
2021-01-23 22:00:18 +0900

    商品情報編集機能(修正)

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -30,7 +30,7 @@ class ProductsController < ApplicationController
     if @product.update(product_params)
       redirect_to product_path
     else
-      render :edit
+      render :index
     end
   end
 
